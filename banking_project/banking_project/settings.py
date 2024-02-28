5d52e53300a996a5fb0c42b9698a2ec6774fef7f
syam2400 <syam2400@gmail.com>
2024-02-28 16:18:51 +0530

    commit

--- banking_project/banking_project/settings.py
+++ banking_project/banking_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&i$15jal)9*ia(v6p5htg-k0k*3tj=*u&40s^@3mmt68@_6l+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
